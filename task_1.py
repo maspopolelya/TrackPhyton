e4de21f589e18622b02df8ab7e2a17151c2e18ee
maspopolelya <danya.sharov.200368@gmail.com>
2026-01-14 00:15:37 +0300

    Лабораторная работа 3

---
+++ task_1.py
@@ -0,0 +1,13 @@
+def find_item_index(list_, item):   #Почему-то пишет incorrect и solution cant be loaded, хотя у моего знакомого выдал correct
+    if item in list_:
+        return list_.index(item)
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
